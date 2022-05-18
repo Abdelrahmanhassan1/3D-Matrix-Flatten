31073ef884c1b3451441c1c10b00563953dd3576
abdelrahmanhassan1 <abdelrahmanwork1@gmail.com>
2022-05-18 22:03:38 +0200

    adding nice classes

--- matrix_flatter.cpp
+++ matrix_flatter.cpp
@@ -17,61 +17,62 @@ class matrix2vector
             height = p;
             size_1d = n * m * p;
             vector_1d = new std::vector<int>(size_1d,0);
-            
-
             vector_3d = new std::vector<std::vector<std::vector<int>>>(depth, std::vector<std::vector<int>>(width, std::vector<int>(height, 0)));
-
         }
-
-        matrix2vector()
+        void initalize_matrix()
         {
-            depth = 0;
-            width = 0;
-            height= 0;
+            for(int i=0;i<depth;i++)
+            {
+                for(int j=0;j<height;j++)
+                {
+                    for(int k=0;k<width;k++)
+                    {
+                        int value;
+                        std::cin>>value;
+                        (*vector_3d)[i][j][k] = value;
+                        int index = get_index(i, j, k);
+                        (*vector_1d)[index] = value;
+                        std::cout<<"index"<<index<<" ";
+                    }     
+                }   
+            }
         }
 
-        // void initalize_matrix()
-        // {
-        //     for(int i=0;i<depth;i++)
-        //     {
-        //         for(int j=0;j<height;j++)
-        //         {
-        //             for(int k=0;k<width;k++)
-        //             {
-        //                 // vector_3d.;
-        //             }   
-        //         }
-        //     }
-        // }
+        void print_matrix()
+        {
+            std::cout<<"---------------:3D Matrix:------------------ \n";
+            for(int i=0;i<depth;i++)
+            {
+                for(int j=0;j<height;j++)
+                {
+                    for(int k=0;k<width;k++)
+                    { 
+                        std::cout << (*vector_3d)[i][j][k] << " ";
+                    }   
+                    std::cout<<"\n";
+                }
+                std::cout<<"\n";
+            }
+            std::cout<<"----------------------------------------\n";
+        }
 
-        // void print_matrix()
-        // {
-        //     for(int i=0;i<depth;i++)
-        //     {
-        //         for(int j=0;j<height;j++)
-        //         {
-        //             for(int k=0;k<width;k++)
-        //             { 
-        //                 std::cout << vector_3d[i][j][k];
-        //             }   
-        //         }
-        //     }
-        // }
-
-        // void set_value(int i, int j, int k, int value)
-        // {
-        //     int index = get_index(i, j, k);
-        //     vector_3d[i][j][k] = value;
-        //     vector_1d[index] = value;
-        // }
+        void set_value(int i, int j, int k, int value)
+        {
+            int index = get_index(i, j, k);
+            (*vector_3d)[i][j][k] = value;
+            (*vector_1d)[index] = value;
+        }
 
         void print_vector()
         {
+            std::cout<<"--------------:1D Vector:---------------- \n";
             for(int i = 0 ; i < size_1d; i++)
             {
-                int temp = (*vector_1d)[i];
-                std::cout<< temp;
+                std::cout<<(*vector_1d)[i]<<" ";
             }
+            std::cout<<"\n";
+            std::cout<<"----------------------------------------\n";
+
         }
 
         int get_index(int i,int j,int k){
@@ -81,10 +82,10 @@ class matrix2vector
 
 int main(void)
 {
-    matrix2vector obj1(2,2,1);
-    // obj1.initalize_matrix();
+    matrix2vector obj1(5,3,2);
+    obj1.initalize_matrix();
     // obj1.set_value(0,0,1, 20);
-    // obj1.print_matrix();
+    obj1.print_matrix();
     obj1.print_vector();
     return 0;
 }
